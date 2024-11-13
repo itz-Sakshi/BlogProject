dfe8aa31aa7c37b5a2451a6ae68a5a10bdd7e248
itz-Sakshi <ghera.sakshi@gmail.com>
2024-11-13 05:20:19 -0500

    test commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { login as authLogin } from '../store/authSlice'
-import {Button, Input, Logo} from "./index"
+import {Button, Logo} from "./index"
+import Input from './index'
 import {useDispatch} from "react-redux"
 import authService from "../appwrite/auth"
 import {useForm} from "react-hook-form"
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import authService from '../appwrite/auth'
 import {Link ,useNavigate} from 'react-router-dom'
 import {login} from '../store/authSlice'
-import {Button, Input, Logo} from './index.js'
+import {Button, Logo} from './index.js'
 import {useDispatch} from 'react-redux'
 import {useForm} from 'react-hook-form'
+import Input from './index.js'
 
 function Signup() {
     const navigate = useNavigate()
